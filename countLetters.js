1ddd63afbbcb2669fb2e547b852ba367746dcf80
AlexWBroughton <ab.alex.broughton@gmail.com>
2023-06-28 23:51:19 +0000

    Counts the number of letters in a given sentece and outputs as an object of letters and letter counts.

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function (actual, expected) {
+  const checkmark = "\u2713";
+  const xmark = "\u274C";
+  if (actual === expected) {
+    console.log(`${checkmark}${checkmark}${checkmark}  Assertion Passed: ${actual} = ${expected}`);
+  }
+  else {
+    console.log(`${xmark}${xmark}${xmark} Assertion Failed: ${actual} != ${expected}`);
+  }
+};
+
+const countLetters = function (countString) {
+  const results = {};
+  let noSpaceString = "";
+  noSpaceString = countString.replace(/ /g, "");
+  for (const letter of noSpaceString) {
+    if (results[letter]) {
+      results[letter] += 1;
+    } else {
+      results[letter] = 1;
+    }
+  }
+  return results;
+};
