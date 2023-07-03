9c1800fc6c15e26bb41d81ebaf6e90bdd82b5bee
AlexWBroughton <ab.alex.broughton@gmail.com>
2023-07-03 20:42:55 +0000

    middleTest is DRY.

---
+++ test/middleTest.js
@@ -0,0 +1,16 @@
+const middle = require("../middle");
+const assertEqual = require("../assertEqual");
+const eqArrays = require("../eqArrays");
+
+
+
+
+
+
+console.log(middle([1])); // => []
+console.log(middle([1, 2])); // => []
+
+console.log(middle([1, 2, 3])); // => [2]
+console.log(middle([1, 2, 3, 4, 5])); // => [3]
+console.log(middle([1, 2, 3, 4])); // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
