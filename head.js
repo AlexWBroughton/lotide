22115905e4a0c7a26ed916377531ad3dea9fbbf6
AlexWBroughton <ab.alex.broughton@gmail.com>
2023-07-03 22:31:59 +0000

    fixed head bug.

--- head.js
+++ head.js
@@ -3,16 +3,18 @@ const assertEqual = require('./assertEqual.js');
 const head = function(arr){
   if (arr.length === 1)
   {
-    if (isNaN(arr)){
+    if (typeof(arr[0] === 'string')){
       return String(arr);
     }
     else{
-      return Number(arr);
+      return Number(arr[0]);
     }
   }
   else if (arr.length > 1){
     return arr[0];
   }
 };
+
+console.log(head(['5']));
 
 module.exports = head;
