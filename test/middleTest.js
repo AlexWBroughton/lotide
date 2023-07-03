fefd5a307ceeac3bcd978523c21daeaa3600692d
AlexWBroughton <ab.alex.broughton@gmail.com>
2023-07-03 22:51:50 +0000

    chai updates to middle, head and tail

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,16 @@
 const middle = require("../middle");
-const assertEqual = require("../assertEqual");
-const eqArrays = require("../eqArrays");
+const assert = require('chai').assert;
 
 
 
+describe("#head", () => {
+  it("returns the middle of element of an array if the length of the array is odd", () => {
+    assert.deepEqual(middle([1, 2, 3,4,5,6,7]), 4);
+  });
+  it("returns the middle two elements of an array if the length of the array is even", () => { 
+    assert.deepEqual(middle([1,2,3,4]), [2,3]); 
+  });
+});
 
 
 
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,4 +1,12 @@
-const assertEqual = require('../assertEqual');
-const tail = require('../tail');
 
-assertEqual(tail([1,2,3]),3);
+const tail = require('../tail');
+const assert = require('chai').assert;
+
+describe("#tail", () => {
+  it("returns 3 for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), 3);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.deepEqual(tail(['5']), '5'); 
+  });
+});
