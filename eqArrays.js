3ee0153cbdb09b3baf1fad08bef329379938acf1
AlexWBroughton <ab.alex.broughton@gmail.com>
2023-07-03 20:18:18 +0000

    eqArrays now DRY

--- eqArrays.js
+++ eqArrays.js
@@ -1,15 +1,4 @@
-const assertEqual = function(actual, expected) {
-  const checkmark = "\u2713";
-  const xmark = "\u274C";
-  if (actual === expected) {
-    console.log(`${checkmark}${checkmark}${checkmark}  Assertion Passed: ${actual} = ${expected}`);
-    return true;
-  }
-  else {
-    console.log(`${xmark}${xmark}${xmark} Assertion Failed: ${actual} != ${expected}`);
-    return false;
-  }
-};  
+assertEqual = require("./assertEqual");
 
 const eqArrays = function (arr1, arr2) {
   let arrBool = false;
@@ -24,12 +13,5 @@ const eqArrays = function (arr1, arr2) {
   return arrBool;
 };
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false*/
-console.log(eqArrays(["1", "2"], ["1", "2", 3]));
-console.log(eqArrays([], ["1", "2", 3]));
-console.log(eqArrays([0, 1], ["0", "1"]));
+module.exports = eqArrays;
 
