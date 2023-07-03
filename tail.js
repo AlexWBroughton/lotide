86e5112391324fabd1faaddae967a75a05fb6fd8
AlexWBroughton <ab.alex.broughton@gmail.com>
2023-07-03 20:25:55 +0000

    fixed a bug in tail now correctly outputs the last element of an array.  It is also DRY

--- tail.js
+++ tail.js
@@ -1,21 +1,15 @@
-const assertEqual = function(actual, expected) {
-  const checkmark = "\u2713";
-  const xmark = "\u274C";
-  if (actual === expected) {
-    console.log(`${checkmark}${checkmark}${checkmark}  Assertion Passed: ${actual} = ${expected}`);
-  }
-  else {
-    console.log(`${xmark}${xmark}${xmark} Assertion Failed: ${actual} != ${expected}`);
-  }
-};  
+const assertEqual = require('./assertEqual');
 
 const tail = function(arr){
   
   if (arr.length >= 1){
-    return arr.slice(1);
+    return arr.pop();
   }
   else{
     return arr;
   }
 }
+
+
+module.exports = tail;
 
